4c1b3ba12af22e26a5f36875d8142985fe35b01a
Joe Putra <39514859+joeputra@users.noreply.github.com>
2024-02-19 09:58:20 +0700

    Tesing Branch

--- lib/views/second_page.dart
+++ lib/views/second_page.dart
@@ -9,7 +9,7 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Second Page"),
+        title: const Text("Allo"),
       ),
       body: SingleChildScrollView(
           child: Center(
